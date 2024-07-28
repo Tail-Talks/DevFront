7379b8d25d45877286f027c0def0a1349bbf103f
@Maival1 <maival.cimba@gmail.com>
2024-07-28 12:52:26 +0200

    feat(buttons): corrected buttons for xl and less media

--- src/components/Main/Preview/index.tsx
+++ src/components/Main/Preview/index.tsx
@@ -17,18 +17,20 @@ export default function Preview() {
           <div className='flex flex-col justify-start w-1/2 mb-auto pt-[3rem]'>
             <h1 className='w-full font-sans text-[#000000] dark:text-white text-6xl font-semibold 3xl:text-7xl'>Социальная сеть Tail Talks</h1>
             <p className='font-Rubik text-[#000000] dark:text-[#F3F3F3] text-xl 3xl:text-3xl font-light mt-10'>Место, где каждый питомец заслуживает быть звездой</p>
-            <ButtonBread
-              name='Узнать подробнее'
-              className='mt-10 mb-10' />
-            <div className='p-[1px] rounded-2xl mr-auto bg-gradient-to-tr from-[#FF4C8C] to-[#FFAE4E]'>
-              <button
-                type='button'
-                className='relative font-sans font-bold text-black-50 dark:text-white text-lg 3xl:text-2xl h-16 3xl:w-96 3xl:h-24 rounded-2xl w-96 flex items-center justify-center bg-white dark:bg-black-50'>
-                Подключить кошелек
-                <img
-                  className='ml-4'
-                  src={theme ? info : infoBlack} alt='info' />
-              </button>
+            <div className='flex gap-5 mt-10 xl:mt-[4rem] flex-col xl:flex-row'>
+              <ButtonBread
+                name='Узнать подробнее'
+                className='w-[310px]' />
+              <div className='p-[1px] rounded-2xl mr-auto bg-gradient-to-tr from-[#FF4C8C] to-[#FFAE4E]'>
+                <button
+                  type='button'
+                  className='relative font-sans font-bold text-black-50 dark:text-white text-lg  h-16 rounded-2xl w-[310px] flex items-center justify-center bg-white dark:bg-black-50'>
+                  Подключить кошелек
+                  <img
+                    className='ml-4'
+                    src={theme ? info : infoBlack} alt='info' />
+                </button>
+              </div>
             </div>
           </div>
 
