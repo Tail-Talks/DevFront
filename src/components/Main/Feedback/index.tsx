610f9320645cd74e81e7685a572c2702cf7ea9c1
@Maival1 <maival.cimba@gmail.com>
2024-07-30 11:52:25 +0200

    feat(feedback): corrected a container for feedback

--- src/components/Main/Feedback/index.tsx
+++ src/components/Main/Feedback/index.tsx
@@ -59,66 +59,70 @@ export default function Feedback() {
   }
 
   return (
-    <section className='feedback flex flex-col mx-auto bg-white dark:bg-transparent sm:dark:bg-[#F3F3F3] sm:shadow-[0_0_20px_rgba(114,114,114,0.1)] rounded-[15px] max-w-[1040px] w-full p-[10px_20px] sm:p-[50px_75px] mt-[70px] sm:mt-[140px]'>
-      <h3 className='font-sans dark:text-white sm:dark:text-black-80 sm:text-black-80 font-medium text-[30px] sm:text-[34px] sm:text-left text-center'>
-        Свяжитесь с нами
-      </h3>
-      <div className='flex flex-col lg:flex-row justify-between w-full gap-[1rem] md:gap-[2.25rem] mt-[1.094rem] sm:mt-[2.188rem]'>
-        <div className='flex flex-col lg:w-[55%]'>
-          <form className='flex flex-col' onSubmit={handleSubmit}>
-            <label htmlFor='name' className='font-Rubic hidden sm:block'>ФИО</label>
-            <input
-              type='text'
-              id='name'
-              name='name'
-              placeholder={isSmallScreen ? 'ФИО' : 'Иванов Иван Иванович'}
-              className='border dark:border-white sm:dark:border-black-80 border-black-80 px-[1.563rem] py-[1.094rem] rounded-xl mt-[1.094rem] sm:mt-[0.625rem] dark:bg-transparent sm:dark:bg-[#F3F3F3] dark:text-white  sm:dark:text-black-80'
-              value={username}
-              onChange={handleUsernameChange}
-            />
-            <label htmlFor='email' className='mt-[1.563rem] font-Rubic hidden sm:block'>Email</label>
-            <input
-              id='email'
-              name='email'
-              type='email'
-              autoComplete='email'
-              placeholder={isSmallScreen ? 'Email' : 'Адрес электронной почты'}
-              className='border dark:border-white sm:dark:border-black-80 border-black-80 px-[1.563rem] py-[1.094rem] rounded-xl mt-[1.094rem] sm:mt-[0.625rem] dark:bg-transparent sm:dark:bg-[#F3F3F3] dark:text-white  sm:dark:text-black-80'
-              value={email}
-              onChange={handleEmailChange} />
-            <label htmlFor='comment' className='mt-[1.563rem] font-Rubic hidden sm:block'>Коментарий</label>
-            <textarea
-              id='comment'
-              name='comment'
-              rows={2}
-              placeholder={isSmallScreen ? 'Коментарий' : ''}
-              className='border dark:border-white sm:dark:border-black-80 border-black-80 px-[1.563rem] py-[1.094rem] rounded-xl mt-[1.094rem] sm:mt-[0.625rem] dark:bg-transparent sm:dark:bg-[#F3F3F3] dark:text-white  sm:dark:text-black-80'
-              value={comment}
-              onChange={handleCommentChange}
-            />
-            <ButtonBread
-              name='Отправить'
-              width="w-full"
-              type='submit'
-              className='mt-[0.625rem]' />
-          </form>
-          <p className='mt-[0.625rem] text-center text-[12px] font-Rubic text-[#7B8694]'>Нажимая на кнопку «Отправить», вы даете согласие на обработку персональных данных</p>
-        </div>
+    <section id='feedback'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col bg-white dark:bg-transparent sm:dark:bg-[#F3F3F3] sm:shadow-[0_0_20px_rgba(114,114,114,0.1)] rounded-[15px] max-w-[1040px] w-full p-[10px_20px] sm:p-[50px_75px] mt-[70px] sm:mt-[140px]'>
+          <h3 className='font-sans dark:text-white sm:dark:text-black-80 sm:text-black-80 font-medium text-[30px] sm:text-[34px] sm:text-left text-center'>
+            Свяжитесь с нами
+          </h3>
+          <div className='flex flex-col lg:flex-row justify-between w-full gap-[1rem] md:gap-[2.25rem] mt-[1.094rem] sm:mt-[2.188rem]'>
+            <div className='flex flex-col lg:w-[55%]'>
+              <form className='flex flex-col' onSubmit={handleSubmit}>
+                <label htmlFor='name' className='font-Rubic hidden sm:block'>ФИО</label>
+                <input
+                  type='text'
+                  id='name'
+                  name='name'
+                  placeholder={isSmallScreen ? 'ФИО' : 'Иванов Иван Иванович'}
+                  className='border dark:border-white sm:dark:border-black-80 border-black-80 px-[1.563rem] py-[1.094rem] rounded-xl mt-[1.094rem] sm:mt-[0.625rem] dark:bg-transparent sm:dark:bg-[#F3F3F3] dark:text-white  sm:dark:text-black-80'
+                  value={username}
+                  onChange={handleUsernameChange}
+                />
+                <label htmlFor='email' className='mt-[1.563rem] font-Rubic hidden sm:block'>Email</label>
+                <input
+                  id='email'
+                  name='email'
+                  type='email'
+                  autoComplete='email'
+                  placeholder={isSmallScreen ? 'Email' : 'Адрес электронной почты'}
+                  className='border dark:border-white sm:dark:border-black-80 border-black-80 px-[1.563rem] py-[1.094rem] rounded-xl mt-[1.094rem] sm:mt-[0.625rem] dark:bg-transparent sm:dark:bg-[#F3F3F3] dark:text-white  sm:dark:text-black-80'
+                  value={email}
+                  onChange={handleEmailChange} />
+                <label htmlFor='comment' className='mt-[1.563rem] font-Rubic hidden sm:block'>Коментарий</label>
+                <textarea
+                  id='comment'
+                  name='comment'
+                  rows={2}
+                  placeholder={isSmallScreen ? 'Коментарий' : ''}
+                  className='border dark:border-white sm:dark:border-black-80 border-black-80 px-[1.563rem] py-[1.094rem] rounded-xl mt-[1.094rem] sm:mt-[0.625rem] dark:bg-transparent sm:dark:bg-[#F3F3F3] dark:text-white  sm:dark:text-black-80'
+                  value={comment}
+                  onChange={handleCommentChange}
+                />
+                <ButtonBread
+                  name='Отправить'
+                  width="w-full"
+                  type='submit'
+                  className='mt-[0.625rem]' />
+              </form>
+              <p className='mt-[0.625rem] text-center text-[12px] font-Rubic text-[#7B8694]'>Нажимая на кнопку «Отправить», вы даете согласие на обработку персональных данных</p>
+            </div>
 
-        <div className='hidden lg:flex lg:w-[45%] relative justify-center items-center'>
-          <img src={ellipse} alt="ellipse" className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' /> {/*Средняя эллипса*/}
-          <img src={ellipse} alt="ellipse" className='absolute top-[calc(50%-1.5rem)] left-1/2 transform -translate-x-1/2 -translate-y-1/2' /> {/*Верхняя эллипса*/}
-          <img src={ellipse} alt="ellipse" className='absolute top-[calc(50%+1.5rem)] left-1/2 transform -translate-x-1/2 -translate-y-1/2' /> {/*Нижняя эллипса*/}
-          <img src={cat} alt='catFB' className='absolute top-[calc(50%-1.1rem)] left-[calc(50%-8rem)] transform -translate-x-1/2 -translate-y-1/2' /> {/*Котик*/}
-          <img src={starColor} alt='starColor' className='absolute top-[calc(50%+8.2rem)] left-[calc(50%-3rem)] transform -translate-x-1/2 -translate-y-1/2' /> {/*Звезда цветная*/}
-          <img src={starGray} alt='starGray' className='absolute top-[calc(50%+1.77rem)] left-[calc(50%+6.4rem)] transform -translate-x-1/2 -translate-y-1/2' /> {/*Звезда серая*/}
-          <img src={starPlain} alt='starPlain' className='absolute top-[calc(50%-7.2rem)] left-[calc(50%+4.5rem)] transform -translate-x-1/2 -translate-y-1/2' /> {/*Звезда простоя*/}
+            <div className='hidden lg:flex lg:w-[45%] relative justify-center items-center'>
+              <img src={ellipse} alt="ellipse" className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' /> {/*Средняя эллипса*/}
+              <img src={ellipse} alt="ellipse" className='absolute top-[calc(50%-1.5rem)] left-1/2 transform -translate-x-1/2 -translate-y-1/2' /> {/*Верхняя эллипса*/}
+              <img src={ellipse} alt="ellipse" className='absolute top-[calc(50%+1.5rem)] left-1/2 transform -translate-x-1/2 -translate-y-1/2' /> {/*Нижняя эллипса*/}
+              <img src={cat} alt='catFB' className='absolute top-[calc(50%-1.1rem)] left-[calc(50%-8rem)] transform -translate-x-1/2 -translate-y-1/2' /> {/*Котик*/}
+              <img src={starColor} alt='starColor' className='absolute top-[calc(50%+8.2rem)] left-[calc(50%-3rem)] transform -translate-x-1/2 -translate-y-1/2' /> {/*Звезда цветная*/}
+              <img src={starGray} alt='starGray' className='absolute top-[calc(50%+1.77rem)] left-[calc(50%+6.4rem)] transform -translate-x-1/2 -translate-y-1/2' /> {/*Звезда серая*/}
+              <img src={starPlain} alt='starPlain' className='absolute top-[calc(50%-7.2rem)] left-[calc(50%+4.5rem)] transform -translate-x-1/2 -translate-y-1/2' /> {/*Звезда простоя*/}
+            </div>
+          </div>
+
+          <SvgSmallFeedback /> {/*SVG для маленьких экранов*/}
+
+          <FormSubmitSuccess isOpen={isModalOpen} closeModal={handleCloseModal} />
         </div>
       </div>
-
-      <SvgSmallFeedback /> {/*SVG для маленьких экранов*/}
-
-      <FormSubmitSuccess isOpen={isModalOpen} closeModal={handleCloseModal} />
     </section>
   )
 }
